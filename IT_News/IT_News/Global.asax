02b97be6d1c7372072e0071fc163482021db6e52
yulia <polushka.yulia@gmail.com>
2019-04-26 23:34:59 +0300

    Start

---
+++ IT_News/IT_News/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IT_News.MvcApplication" Language="C#" %>
---
+++ IT_News/IT_News/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IT_News.Startup))]
+namespace IT_News
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
