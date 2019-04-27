c97b1ff8001bed916c6ce39e81a3ee0434cf3bd2
yulia <polushka.yulia@gmail.com>
2019-04-27 08:44:52 +0300

    commit

--- IT_News/IT_News/Global.asax.cs
+++ IT_News/IT_News/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using IT_News.Models;
 
 namespace IT_News
 {
@@ -12,6 +14,8 @@ namespace IT_News
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ IT_News/IT_News/Models/AppDbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IT_News.Models
+{
+    public class AppDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            // создаем две роли
+            var role1 = new IdentityRole { Name = "admin" };
+            var role2 = new IdentityRole { Name = "guest" };
+            var role3 = new IdentityRole { Name = "reader" };
+            var role4 = new IdentityRole { Name = "writer" };
+
+            // добавляем роли в бд
+            roleManager.Create(role1);
+            roleManager.Create(role2);
+            roleManager.Create(role3);
+            roleManager.Create(role4);
+
+            // создаем пользователей
+            var admin = new ApplicationUser { Email = "polushka.yulia@gmai.com", UserName = "Poluhina" };
+            string password = "2502666z";
+            var result = userManager.Create(admin, password);
+
+            // если создание пользователя прошло успешно
+            if (result.Succeeded)
+            {
+                // добавляем для пользователя роль
+                userManager.AddToRole(admin.Id, role1.Name);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
